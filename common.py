9da252b180f3ae8955add5980e5b83ae7fa08d76
Wildric <wildric.auric@gmail.com>
2023-09-17 04:17:41 +0200

    refactored code, updated to slash commands

---
+++ common.py
@@ -0,0 +1,10 @@
+BOT_TOKEN                       = "MTAwNTk2MTM5Njg2Mjg1MzIxMg.Gp9Dzu.dEk1zAqD1fUH6YN2z5gkKE7X-z3eWlDbVJLyl8"
+SERVER_ID                       = 1151212134471045253
+DEFAULT_CHANNEL_ID              = 1151217161247932437
+ADMIN_ROLE                      = "Bureau"
+
+
+
+#DONT EDIT THE FOLLOWING!
+gInvalidCmdMsg = "Hmm, weird bug; if you see this msg contact the dev"
+gDB            = None #global database
